d8e4e7e6ab9e7ef2039b81e81d9b57e7b013cf0a
Abhi <abhishekwankhade4670@gmail.com>
2022-08-05 21:30:09 +0530

    ch2 complete

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,24 @@ class Homepage extends StatelessWidget {
       appBar: AppBar(
         title: Text('first app'),
       ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(colors: [Colors.pink, Colors.yellow]),
+              color: Colors.yellow,
+              boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 20)]),
+          child: Text(
+            'Click me  ',
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+        ),
+      ),
     );
   }
 }
